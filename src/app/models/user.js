9ca0be4a5c3471d6e35c6a6ec20615d9f8319a79
Jason <azoulayjason@gmail.com>
2019-04-13 16:42:28 +0200

    #1 Implement auth: methods for user model

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -16,4 +16,34 @@ var UserSchema = new mongoose.Schema({
     }
 })
 
+
+// pre save schema done before saving to database
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if(!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    })
+})
+
+// if the user logs later on  need a safe way to compare passwords
+
+//TODO check about arrows functions
+UserSchema.methods.comparePassword = function(candidatePassword, cb) { // and not here 
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => { // why use arrow function
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
 module.exports = mongoose.model('User', UserSchema);
