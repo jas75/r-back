461614ba4689c6d1faff49fd2fe057614581834e
Jason <azoulayjason@gmail.com>
2019-04-13 16:22:07 +0200

    #1 Implement auth: refactor and create user model

---
+++ src/app/models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema);
